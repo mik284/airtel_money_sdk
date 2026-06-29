fcc9cacba9ee867a42aba5abcc9d3d1812fbff9e
mik284 <mikeykamau222@gmail.com>
2026-06-29 23:16:55 +0300

    chore: bump version to 0.1.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule AirtelMoney.MixProject do
   def project do
     [
       app: :airtel_money,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.15",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
